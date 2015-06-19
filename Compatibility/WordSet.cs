f2d4c038146cce460f7230a0d0e7aa4cb3f08133
otuncelli <tuncelli@gmail.com>
2015-06-19 15:42:16 +0300

    Initial commit.

---
+++ Compatibility/WordSet.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+
+namespace TurkishDeasciifier.Compatibility
+{
+    internal class WordSet<T> :  ICollection<T>
+    {
+        private readonly Dictionary<T, object> d;
+        public WordSet(int capacity)
+        {
+            d = new Dictionary<T, object>(capacity);
+        }
+
+        public WordSet()
+            : this(0)
+        {
+        }
+
+        public WordSet(IEnumerable<T> items)
+            : this()
+        {
+            if (items == null) { return; }
+            foreach (T item in items)
+            {
+                Add(item);
+            }
+        }
+
+        public void Add(T item)
+        {
+            d.Add(item, null);
+        }
+
+        public void Clear()
+        {
+            d.Clear();
+        }
+
+        public bool Contains(T item)
+        {
+            return d.ContainsKey(item);
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            d.Keys.CopyTo(array, arrayIndex);
+        }
+
+        public int Count
+        {
+            get { return d.Count; }
+        }
+
+        public bool IsReadOnly
+        {
+            get { return false; }
+        }
+
+        public bool Remove(T item)
+        {
+            return d.Remove(item);
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return d.Keys.GetEnumerator();
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ Deasciifier.cs
@@ -0,0 +1,320 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Reflection;
+using System.Text;
+
+namespace TurkishDeasciifier
+{
+    using Compatibility;
+    public class Deasciifier
+    {
+        public const int DEFAULT_CONTEXTSIZE = 20;
+
+        #region Private Values
+        private readonly int contextSize;
+        private readonly bool aggressive;
+        private WordSet<string> exclusions;
+        private Dictionary<string, string> corrections;
+        private string originalString;
+        private char[] buffer;
+        #endregion
+
+        #region Constructors
+        public Deasciifier() : this(DEFAULT_CONTEXTSIZE, false) { }
+
+        public Deasciifier(int contextSize, bool aggressive)
+        {
+            this.contextSize = contextSize;
+            this.aggressive = aggressive;
+            //LoadExclusions();
+            //LoadCorrections();
+        }
+        #endregion
+
+        #region Public Methods
+        /// <summary>
+        /// Asciifies the input
+        /// </summary>
+        /// <param name="turkishString">turkish input string with non-ascii letters</param>
+        /// <returns>asciified string</returns>
+        public static string Asciify(string turkishString)
+        {
+            StringBuilder asciified = new StringBuilder();
+            string normalized = turkishString.Normalize(NormalizationForm.FormD);
+            foreach (char ch in normalized)
+            {
+                if (ch == '\u0131')
+                    asciified.Append('i');
+                else if (CharUnicodeInfo.GetUnicodeCategory(ch) != UnicodeCategory.NonSpacingMark)
+                    asciified.Append(ch);
+            }
+            return asciified.ToString();
+        }
+
+        /// <summary>
+        /// Deasciifies the input
+        /// </summary>
+        /// <param name="asciiString">ascii string</param>
+        /// <returns>deasciified string</returns>
+        public string DeAsciify(string asciiString)
+        {
+            if (asciiString == null) { throw new ArgumentNullException("asciiString"); }
+            asciiString += " ";
+            return DeAsciify(asciiString, 0, asciiString.Length);
+        }
+
+        /// <summary>
+        /// Deasciifies the specified input region
+        /// </summary>
+        /// <param name="asciiString">ascii string</param>
+        /// <param name="startIndex">region start index</param>
+        /// <param name="length">region size</param>
+        /// <returns></returns>
+        public string DeAsciify(string asciiString, int startIndex, int length)
+        {
+            if (string.IsNullOrEmpty(asciiString) || asciiString.Trim().Length == 0)
+            {
+                return asciiString;
+            }
+
+            if (exclusions != null)
+            {
+                CheckExclusions(asciiString);
+            }
+
+            this.originalString = asciiString;
+            buffer = asciiString.ToCharArray(startIndex, length);
+            for (var i = startIndex; i < length; i++)
+            {
+                char ch = buffer[i], x;
+                if (NeedCorrection(ch, i) && DeasciifierPatterns.TurkishToggleAccentsTable.TryGetValue(ch, out x))
+                {
+                    // Adds or removes turkish accent at the cursor.
+                    SetCharAt(ref buffer, i, x);
+                }
+            }
+            
+            asciiString = new string(buffer);
+            if (exclusions != null)
+            {
+                asciiString = ApplyExclusions(asciiString);
+            }
+            if (corrections != null)
+            {
+                asciiString = ApplyCorrections(asciiString);
+            }
+            return asciiString;
+        }
+        #endregion
+
+        #region Private Methods
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2202:Do not dispose objects multiple times")]
+        private static string[] LoadList(string resourceName)
+        {
+            List<string> list = new List<string>();
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            resourceName = string.Join(".", new string[] { assembly.GetName().Name, "Resources", resourceName });
+            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+            using (TextReader reader = new StreamReader(stream))
+            {
+                string line;
+                while (!string.IsNullOrEmpty(line = reader.ReadLine()) && line.Trim().Length > 0)
+                {
+                    list.Add(line.Trim().ToLower());
+                }
+            }
+            return list.ToArray();
+        }
+
+        struct WordPosition
+        {
+            internal int StartIndex;
+            internal int EndIndex;
+        }
+
+        private List<WordPosition> exclusionPositions;
+        private void CheckExclusions(string input)
+        {
+            exclusionPositions = new List<WordPosition>();
+            foreach (string exc in exclusions)
+            {
+                int idx;
+                if ((idx = input.IndexOf(exc)) > -1 &&
+                    ((idx > 0 && input[idx-1] == ' ') &&
+                    (idx + exc.Length < input.Length && input[idx + exc.Length] == ' ')))
+                {
+                    exclusionPositions.Add(new WordPosition(){StartIndex=idx, EndIndex=idx+exc.Length});
+                }
+            }
+        }
+
+        private string ApplyExclusions(string output)
+        {
+            StringBuilder sb = new StringBuilder(output);
+            foreach (var exc in exclusionPositions)
+            {
+                for (int i = exc.StartIndex; i < exc.EndIndex; i++)
+                {
+                    sb[i] = originalString[i];
+                }
+            }
+            return sb.ToString();
+        }
+
+        private void LoadExclusions()
+        {
+            string[] list = LoadList("exceptions.txt");
+            exclusions = new WordSet<string>(list);
+        }
+
+        private string ApplyCorrections(string output)
+        {
+            StringBuilder sb = new StringBuilder(output);
+            foreach (KeyValuePair<string,string> kvp in corrections)
+            {
+                int idx;
+                int len = kvp.Key.Length;
+                if ((idx = output.IndexOf(kvp.Key)) > -1 &&
+                    ((idx > 0 && output[idx - 1] == ' ') &&
+                    (idx + len < output.Length && output[idx + len] == ' ')))
+                {
+                    for (int i = idx, j =0; i < idx + len; i++)
+                    {
+                        sb[i] = kvp.Value[j++];
+                    }
+                }
+            }
+            return sb.ToString();
+        }
+
+        private void LoadCorrections()
+        {
+            string[] list = LoadList("corrections.txt");
+            corrections = new Dictionary<string, string>(list.Length);
+            foreach (string line in list)
+            {
+                string[] w2w = line.Split(',');
+                w2w[0] = w2w[0].Trim().ToLower();
+                w2w[1] = w2w[1].Trim().ToLower();
+                corrections.Add(w2w[0], w2w[1]);
+            }
+        }
+
+        private static void SetCharAt(ref char[] buf, int index, char ch)
+        {
+            buf[index] = ch;
+        }
+
+        /// <summary>
+        /// Determine if char at cursor needs correction.
+        /// </summary>
+        /// <param name="ch">char</param>
+        /// <param name="point">index</param>
+        /// <returns>whether if needs correction</returns>
+        private bool NeedCorrection(char ch, int point)
+        {
+            char tr;
+            if (!DeasciifierPatterns.TurkishAsciifyTable.TryGetValue(ch, out tr))
+                tr = ch;
+            else if (!aggressive)
+                return false; // aslı & asli problemi
+
+            bool m = false;
+            Dictionary<string, short> pattern;
+            if (DeasciifierPatterns.TryGetPattern(tr, out pattern))
+            {
+                m = MatchPattern(pattern, point);
+            }
+
+            if (tr == 'I')
+                return (ch == tr) ? !m : m;
+            return (ch == tr) ? m : !m;
+        }
+
+        private bool MatchPattern(Dictionary<string, short> pattern, int point)
+        {
+            char[] s = GetContext(contextSize, point);
+            int rank = pattern.Count * 2;
+            int start = 0;
+            while (start <= contextSize)
+            {
+                int end = contextSize + 1;
+                while (end <= s.Length)
+                {
+                    string str = new string(s, start, end - start);
+                    short r;
+                    if (pattern.TryGetValue(str, out r) && Math.Abs(r) < Math.Abs(rank))
+                    {
+                        rank = r;
+                    }
+                    end++;
+                }
+                start++;
+            }
+            return rank > 0;
+        }
+
+        private char[] GetContext(int size, int point)
+        {
+            char[] s = new string(' ', 1 + (2 * size)).ToCharArray();
+            SetCharAt(ref s, size, 'X');
+            int i = size + 1;
+            int index = point + 1;
+            bool space = false;
+
+            while (i < s.Length && !space && index < buffer.Length)
+            {
+                char cc = buffer[index];
+                char x;
+                if (!DeasciifierPatterns.TurkishDowncaseAsciifyTable.TryGetValue(cc, out x))
+                {
+                    if (!space)
+                    {
+                        i++;
+                        space = true;
+                    }
+                }
+                else
+                {
+                    SetCharAt(ref s, i, x);
+                    i++;
+                    space = false;
+                }
+                //i++;
+                index++;
+            }
+
+            Array.Resize(ref s, i);
+            index = point;
+            i = size - 1;
+            space = false;
+            index--;
+
+            while (i >= 0 && index >= 0)
+            {
+                char cc = buffer[index];
+                char x;
+                if (!DeasciifierPatterns.TurkishUpcaseAccentsTable.TryGetValue(cc, out x))
+                {
+                    if (!space)
+                    {
+                        i--;
+                        space = true;
+                    }
+                }
+                else
+                {
+                    SetCharAt(ref s, i, x);
+                    i--;
+                    space = false;
+                }
+                //i--;
+                index--;
+            }
+            return s;
+        }
+        #endregion
+    }
+}
